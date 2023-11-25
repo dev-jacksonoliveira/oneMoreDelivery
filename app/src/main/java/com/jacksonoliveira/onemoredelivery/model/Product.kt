8eb9b5fdfced44cb58933255a5b7be391d529a81
Jackson Oliveira <jackson_zappts@getnet.com.br>
2023-11-25 14:34:29 -0300

    feature: create class of Product

---
+++ app/src/main/java/com/jacksonoliveira/onemoredelivery/model/Product.kt
@@ -0,0 +1,10 @@
+package com.jacksonoliveira.onemoredelivery.model
+
+import androidx.annotation.DrawableRes
+import java.math.BigDecimal
+
+class Product(
+    val name: String,
+    val price: BigDecimal,
+    @DrawableRes val image: Int
+)
